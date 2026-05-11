3fa8012123ecce1a68b21c0c4f5525a5ef448ab8
Cedrik Hoffmann <choffmann@progeek.de>
2026-05-11 08:36:33 +0200

    fix: update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,8 @@ return {
 	lazy = false,
 	build = ":TSUpdate",
 	config = function()
+		local nts = require("nvim-treesitter")
+
 		local parsers = {
 			-- Core / editor
 			"vim",
@@ -63,11 +65,40 @@ return {
 			"git_rebase",
 		}
 
-		require("nvim-treesitter").install(parsers)
+		nts.install(parsers)
+
+		-- Auto-install on first encounter of an unknown filetype.
+		local available = {}
+		for _, p in ipairs(nts.get_available()) do
+			available[p] = true
+		end
+		local tried = {}
 
 		vim.api.nvim_create_autocmd("FileType", {
 			callback = function(args)
-				pcall(vim.treesitter.start, args.buf)
+				local ft = vim.bo[args.buf].filetype
+				if ft == "" then
+					return
+				end
+
+				local lang = vim.treesitter.language.get_lang(ft) or ft
+
+				if pcall(vim.treesitter.start, args.buf, lang) then
+					return
+				end
+
+				if tried[lang] or not available[lang] then
+					return
+				end
+				tried[lang] = true
+
+				nts.install(lang):await(function()
+					vim.schedule(function()
+						if vim.api.nvim_buf_is_valid(args.buf) then
+							pcall(vim.treesitter.start, args.buf, lang)
+						end
+					end)
+				end)
 			end,
 		})
 	end,
