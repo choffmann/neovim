3b47210e8a6d78d4bd53ae43f45f5706d1db7ff5
Cedrik Hoffmann <dev@choffmann.io>
2026-07-10 20:25:32 +0200

    refactor: drop manual runtimepath hack for stylix colors
    The palette is now generated into data/site/lua, which is on the
    runtimepath by default and survives lazy's rtp reset.

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,7 +1,3 @@
--- Add stylix colors path
-vim.opt.rtp:append(vim.fn.stdpath("data"))
-package.path = package.path .. ";" .. vim.fn.stdpath("data") .. "/lua/?.lua"
-
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
